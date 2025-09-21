22d7d0b64802c61facfc4c0584051509456d7489
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-20 19:33:40 -0500

    create logger.go with initial log implementations

---
+++ logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"log"
+)
+
+//// Logger wraps logging functionality behind a boolean toggle
+//
+type Logger struct {
+	enabled bool
+}
+//
+////
+
+//// Create a new Logger instance
+// 
+func NewLogger(enabled bool) *Logger {
+	return &Logger { enabled: enabled }
+}
+// 
+////
+
+//// Wrapping log.Print
+//
+func (logger *Logger) Print(args ...interface{}) {
+	if logger.enabled = true {
+		log.Print(args)
+	}
+}	
+//
+////
+
+//// Wrapping log.Printf
+//
+func (logger *Logger) Printf(format string, args ...interface{}) {
+	if logger.enabled == true {
+		log.Printf(format, args)
+	}
+}	
+//
+////
+
